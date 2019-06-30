1d4988eee5fe1acde2546cac9ef347fdeb9b1ac5
Bukov <dmitrij.buckov@yandex.ru>
2019-06-30 23:05:34 +0300

    home edition

--- src/AppBundle/Controller/ServiceController.php
+++ src/AppBundle/Controller/ServiceController.php
@@ -2,26 +2,34 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Cartridge;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+use AppBundle\Entity\Task;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
 class ServiceController extends Controller
 {
     /**
      * @Route("/service", name="service_list" )
-     * @Template()
      */
     public function indexAction(Request $request)
     {
         $services = [
-            '0' => 'Cartridges',
-            '1' => 'Repair',
-            '2' => 'Consultation'
+            '0' => 'Заявки на картриджи',
+            '1' => 'Ремонт техники',
+            '2' => 'Консультация',
+            '3' => 'Заявка на выезд',
         ];
-        return  ['services' => $services];
+        return  $this->render('@App/service/index.html.twig');
     }
 
     /**
@@ -32,9 +40,10 @@ class ServiceController extends Controller
     public function showAction($id)
     {
         $services = [
-            '0' => 'Cartridges',
-            '1' => 'Repair',
-            '2' => 'Consultation'
+            '0' => '1',
+            '1' => 'Ремонт техники',
+            '2' => 'Консультация',
+            '3' => 'Заявка на выезд',
         ];
 
         return $this->render('@App/service/show.html.twig',[
@@ -43,4 +52,126 @@ class ServiceController extends Controller
         ]);
     }
 
+    /**
+     * @Route("/service/cartridge", name="service_cartridge")
+     */
+    public function cartridgeAction()
+    {
+        $task = new Cartridge();
+        //$task->setDistrict('Укажте район');
+
+
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            //->add('task', TextType::class, ['label' => 'Укажите причину обращения'])
+            ->add('district', ChoiceType::class, array(
+                'choices'  => array(
+                    'Данков' => null,
+                    'Доброе' => true,
+                    'Измалково' => false,
+                    'Красное' => null,
+                    'Лебедянь' => true,
+                    'Липецкий' => false,
+                    'Становое' => null,
+                    'Долгоруково' => true,
+                    'Усмань' => false,
+                    'Хлевное' => null,
+                    'Тербуны' => true,
+                    'Волово' => false,
+                    'Лев-Толстой' => null,
+                    'Чаплыгин' => true,
+                    'Добринка' => false,
+                    'Задонск' => false,
+                )))
+            ->add('department', TextType::class, ['label' => 'Отдел'])
+            ->add('cartridgeModel', TextType::class, ['label' => 'Модель картриджа'])
+            ->add('printerModel', TextType::class, ['label' => 'Модель принтера'])
+            ->add('dueDate', DateType::class, ['label' => 'На какой месяц заявка'])
+            ->add('isAttending', ChoiceType::class, array(
+                'choices'  => array(
+                    'Maybe' => null,
+                    'Yes' => true,
+                    'No' => false,
+                ),
+            ))
+            ->add('save', SubmitType::class, ['label' => 'Отправить заявку'])
+            ->getForm();
+
+        return $this->render('@App/service/cartridges.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/service/repair", name="service_repair")
+     */
+    public function repairAction()
+    {
+        $task = new Task();
+        $task->setTask('Укажте причину обращения');
+        $task->setDistrict('Укажте район');
+
+
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class, ['label' => 'Укажите причину обращения'])
+            ->add('district', TextType::class, ['label' => 'Район'])
+            ->add('department', TextType::class, ['label' => 'Отдел'])
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        return $this->render('@App/service/repair.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/service/consultation", name="service_consultation")
+     */
+    public function consultationAction()
+    {
+        $task = new Task();
+        $task->setTask('Укажте причину обращения');
+        $task->setDistrict('Укажте район');
+
+
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class, ['label' => 'Укажите причину обращения'])
+            ->add('district', TextType::class, ['label' => 'Район'])
+            ->add('department', TextType::class, ['label' => 'Отдел'])
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        return $this->render('@App/service/consultation.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/service/departure", name="service_departure")
+     */
+    public function departureAction()
+    {
+        $task = new Task();
+        $task->setTask('Укажте причину обращения');
+        $task->setDistrict('Укажте район');
+        $task->setDueDate(new \DateTime('tomorrow'));
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class, ['label' => 'Укажите причину обращения'])
+            ->add('district', TextType::class, ['label' => 'Район'])
+            ->add('department', TextType::class, ['label' => 'Отдел'])
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        return $this->render('@App/service/departure.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
