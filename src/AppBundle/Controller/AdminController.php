e5549939e2c36731065af7acbcafed1623f92e8f
Bukov <dmitrij.buckov@yandex.ru>
2019-07-17 23:41:12 +0300

    grid

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -41,4 +41,23 @@ class AdminController extends ApplicationController
         return $grid->getGridResponse('@App/admin/grid.html.twig');
     }
 
+    /**
+     * @Route("/admin_grid2", name="admin_grid2")
+     */
+    public function gridAction2()
+    {
+        // Creates a simple grid based on your entity (ORM)
+        $source = new Entity('AppBundle:CartridgeOrder');
+
+        // Get a Grid instance
+        $grid = $this->get('grid');
+
+        // Attach the source to the grid
+        $grid->setSource($source);
+
+
+        // Return the response of the grid to the template
+        return $grid->getGridResponse('@App/admin/grid.html.twig');
+    }
+
 }
